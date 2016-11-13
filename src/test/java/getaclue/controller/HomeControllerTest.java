8566cc11bbc77e6b500a82be9607f364789e0313
Thomas Senke <tsenke@gmail.com>
2016-11-12 21:39:37 -0500

    Remove unused import

--- src/test/java/getaclue/controller/HomeControllerTest.java
+++ src/test/java/getaclue/controller/HomeControllerTest.java
@@ -8,7 +8,6 @@ import static org.springframework.security.test.web.servlet.response.SecurityMoc
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;

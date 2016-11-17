8504b2373a53500dc64ce9f6ec7a513216e31c48
Thomas Senke <tsenke@gmail.com>
2016-11-17 14:47:15 -0500

    CLUEL-17 Build deck of cards

--- src/main/java/getaclue/domain/Card.java
+++ src/main/java/getaclue/domain/Card.java
@@ -1,5 +1,8 @@
 package getaclue.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 
@@ -8,8 +11,31 @@ import javax.validation.constraints.NotNull;
  */
 @Entity
 public final class Card extends PersistableGameObject {
-
     private static final long serialVersionUID = -2305395887705384349L;
+
+    private static final List<Card> PROTO_DECK = new ArrayList<>();
+
+    // Initialize the prototype deck
+    static {
+        for (Guest guest : Guest.values()) {
+            PROTO_DECK.add(new Card(guest.getName()));
+        }
+        for (Room room : Room.values()) {
+            PROTO_DECK.add(new Card(room.getName()));
+        }
+        for (Weapon weapon : Weapon.values()) {
+            PROTO_DECK.add(new Card(weapon.getName()));
+        }
+    }
+
+    /**
+     * Get a new deck of cards.
+     *
+     * @return a list of all cards
+     */
+    public static ArrayList<Card> newDeck() {
+        return new ArrayList<Card>(PROTO_DECK);
+    }
 
     @NotNull
     private String name;

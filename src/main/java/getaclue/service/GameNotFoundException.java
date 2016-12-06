b37956eba6c90b1bc60238f9ae6be4cd2f690491
Thomas Senke <tsenke@gmail.com>
2016-12-06 10:47:40 -0500

    Add ResponseStatus annotation to exception classes

--- src/main/java/getaclue/service/GameNotFoundException.java
+++ src/main/java/getaclue/service/GameNotFoundException.java
@@ -1,9 +1,13 @@
 package getaclue.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Exception thrown when the user requests an action against a game that doesn't
  * exist in the repository.
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public final class GameNotFoundException extends Exception {
     private static final long serialVersionUID = 4739089592582336211L;
 
--- src/main/java/getaclue/service/InvalidGameStateException.java
+++ src/main/java/getaclue/service/InvalidGameStateException.java
@@ -1,10 +1,14 @@
 package getaclue.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Exception thrown when the user requests an action against a game that is in
  * the wrong state. For example, the user tries to join a game that is already
  * in progress.
  */
+@ResponseStatus(HttpStatus.CONFLICT)
 public final class InvalidGameStateException extends Exception {
     private static final long serialVersionUID = -3341631815800097440L;
 

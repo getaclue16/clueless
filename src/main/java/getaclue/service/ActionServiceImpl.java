53d8abc82baaaa3f2d4dee568ab5c0b6026263eb
Thomas Senke <tsenke@gmail.com>
2016-12-06 14:38:32 -0500

    Reword error message

--- src/main/java/getaclue/service/ActionServiceImpl.java
+++ src/main/java/getaclue/service/ActionServiceImpl.java
@@ -58,8 +58,8 @@ public class ActionServiceImpl implements ActionService {
         }
         // Make sure the move is legal
         if (!Location.isValidMove(player.getCurrentLocation(), moveto)) {
-            throw new InvalidGameStateException(String.format("It is invalid to move from %s to %s",
-                    player.getCurrentLocation(), moveto));
+            throw new InvalidGameStateException(
+                    String.format("That is not a valid move", player.getCurrentLocation(), moveto));
         }
         // If the target is a hall, make sure it is empty
         if (moveto.isHall()) {
